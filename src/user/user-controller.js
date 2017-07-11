98f546a49cc8d87e6bc3d6e1955bc287e6bb6f41
Wattana Ruengmucha <wattana_r@deestonegrp.com>
2017-07-11 09:58:10 +0700

    update user func name

--- src/user/user-controller.js
+++ src/user/user-controller.js
@@ -1,4 +1,4 @@
-module.exports.User = (request, reply) => {
+module.exports.Home = (request, reply) => {
 	return  reply.view('index', {
 		title: 'User Page',
 		message: 'Hello World!'
--- src/user/user-route.js
+++ src/user/user-route.js
@@ -1,5 +1,5 @@
 const UserController = require('./user-controller');
 
 module.exports = [
-	{ method: 'GET', path: '/user', handler: UserController.User }
+	{ method: 'GET', path: '/user', handler: UserController.Home }
 ];
